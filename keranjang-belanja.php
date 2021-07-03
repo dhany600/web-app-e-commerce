757910be23d6748901d809b40d12addc1ef85a5b
dhany600 <dhanymartin@gmail.com>
2021-07-03 21:51:58 +0700

    anjas

--- keranjang-belanja.php
+++ keranjang-belanja.php
@@ -3,8 +3,74 @@
 <section class="section-1 shopping-cart-list">
     <div class="prelative">
         <h3 class="content-title">
-            
+            Keranjang Belanja
         </h3>
+        <div class="row header-table-row">
+            <div class="col-md-2 initial-table-border">
+                <div class="header-table">
+                    Gambar
+                </div>
+            </div>
+            <div class="col-md-4 initial-table-border">
+                <div class="header-table">
+                    Nama
+                </div>
+            </div>
+            <div class="col-md-1 initial-table-border">
+                <div class="header-table">
+                    Quantity
+                </div>
+            </div>
+            <div class="col-md-2 initial-table-border">
+                <div class="header-table">
+                    Harga Barang
+                </div>
+            </div>
+            <div class="col-md-2 initial-table-border">
+                <div class="header-table">
+                    Harga Total
+                </div>
+            </div>
+            <div class="col-md-1 last-table-border">
+                <div class="header-table">
+                    Edit Barang
+                </div>
+            </div>
+        </div>
+        <div class="row content-table-row">
+            <div class="col-md-2 initial-table-border">
+                <div class="header-table">
+                    Gambar
+                </div>
+            </div>
+            <div class="col-md-4 initial-table-border">
+                <div class="header-table">
+                    Nama
+                </div>
+            </div>
+            <div class="col-md-1 initial-table-border">
+                <div class="header-table">
+                    Quantity
+                </div>
+            </div>
+            <div class="col-md-2 initial-table-border">
+                <div class="header-table">
+                    Harga Barang
+                </div>
+            </div>
+            <div class="col-md-2 initial-table-border">
+                <div class="header-table">
+                    Harga Total
+                </div>
+            </div>
+            <div class="col-md-1 last-table-border">
+                <div class="header-table">
+                    <button type="button" class="btn btn-primary">
+                        <i class="fas fa-trash-alt"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 
