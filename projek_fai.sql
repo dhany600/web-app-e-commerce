d8b10862839df1d98a51f252d3be8f902da9eae6
Evan-Pram <tepenpramono@yahoo.com>
2021-07-07 22:46:57 +0700

    sql databasenya ketinggalan

--- projek_fai.sql
+++ projek_fai.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 06, 2021 at 07:14 PM
+-- Generation Time: Jul 07, 2021 at 03:46 PM
 -- Server version: 5.7.33
 -- PHP Version: 7.4.19
 
@@ -52,24 +52,43 @@ INSERT INTO `barang` (`ID`, `Nama`, `Deskripsi`, `Kategori`, `Harga`, `Stok`, `t
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `detail_tranksaksi`
+--
+
+CREATE TABLE `detail_tranksaksi` (
+  `ID_tranksaksi` varchar(20) NOT NULL,
+  `ID_Barang` int(11) NOT NULL,
+  `jumlah_barang` int(11) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `detail_tranksaksi`
+--
+
+INSERT INTO `detail_tranksaksi` (`ID_tranksaksi`, `ID_Barang`, `jumlah_barang`, `harga`) VALUES
+('820210707150751', 1, 2, 250000),
+('820210707150701', 1, 2, 250000),
+('1320210707200701', 1, 2, 250000);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `keranjang_belanja`
 --
 
 CREATE TABLE `keranjang_belanja` (
   `ID_user` int(11) NOT NULL,
   `ID_Barang` int(11) NOT NULL,
-  `jumlah_barang` int(11) NOT NULL,
-  `status` int(11) DEFAULT '0'
+  `jumlah_barang` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `keranjang_belanja`
 --
 
-INSERT INTO `keranjang_belanja` (`ID_user`, `ID_Barang`, `jumlah_barang`, `status`) VALUES
-(11, 1, 1, 0),
-(11, 3, 2, 0),
-(13, 1, 5, 0);
+INSERT INTO `keranjang_belanja` (`ID_user`, `ID_Barang`, `jumlah_barang`) VALUES
+(8, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -78,18 +97,23 @@ INSERT INTO `keranjang_belanja` (`ID_user`, `ID_Barang`, `jumlah_barang`, `statu
 --
 
 CREATE TABLE `tranksaksi` (
-  `ID` varchar(20) NOT NULL,
+  `ID` varchar(25) NOT NULL,
+  `ID_user` int(11) NOT NULL,
   `Total_Harga` int(11) NOT NULL,
   `Date` varchar(25) NOT NULL,
-  `Status` int(1) NOT NULL
+  `Status` int(1) DEFAULT '0',
+  `bukti_transfer` varchar(100) DEFAULT NULL,
+  `no_resi` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tranksaksi`
 --
 
-INSERT INTO `tranksaksi` (`ID`, `Total_Harga`, `Date`, `Status`) VALUES
-('3', 5000000, '2021-07-06 17:07:50', 3);
+INSERT INTO `tranksaksi` (`ID`, `ID_user`, `Total_Harga`, `Date`, `Status`, `bukti_transfer`, `no_resi`) VALUES
+('820210707150751', 8, 250000, '2021-07-07 15:07:51', 2, '820210707150751.png', '123456'),
+('820210707150701', 8, 250000, '2021-07-07 15:07:01', 2, '820210707150701.png', '123456'),
+('1320210707200701', 13, 250000, '2021-07-07 20:07:01', 1, '1320210707200701.jpg', '');
 
 -- --------------------------------------------------------
 
@@ -118,7 +142,7 @@ INSERT INTO `user` (`username`, `password`, `email`, `kecamatan`, `Kode_Pos`, `a
 (NULL, '$2y$10$ukhwOjyoO1KaiN8fyc7u7.KMyMvNIPnRyKLTrwMC0sJSx309Qd1CC', 'test1@test.test', NULL, NULL, NULL, NULL, 0, 9),
 ('bagong', '$2y$10$GCe8aU.C8eBag4/muy7wbu5JR8iBgoQm/RFxPP8rcS0v6cEqXzjCG', 'dimas78@gmail.com', NULL, NULL, NULL, NULL, 0, 10),
 ('admin', '$2y$10$xxLnPBIh5D/dDifB./EB/em.HUnPOl3hSsS8DdGPYo8nBK.bYYGG2', 'admin@admin.admin', NULL, NULL, NULL, NULL, 1, 11),
-('steve__74', '$2y$10$Kilho1APbfMPjqaw0aOyPev4CfxhPv6rOfBShYFdSI8OcF1wlwQI2', 'steve696974@gmail.com', NULL, NULL, NULL, NULL, 0, 13);
+('steve__74', '$2y$10$Kilho1APbfMPjqaw0aOyPev4CfxhPv6rOfBShYFdSI8OcF1wlwQI2', 'steve696974@gmail.com', NULL, NULL, 'lol', 123456, 0, 13);
 
 --
 -- Indexes for dumped tables
