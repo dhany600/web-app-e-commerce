0f9359ad68bdab7c8e80dea1475a7cb111d7f471
dhany600 <dhanymartin@gmail.com>
2021-07-07 23:35:15 +0700

    pushing

--- admin-cek-transfer-detail.php
+++ admin-cek-transfer-detail.php
@@ -199,17 +199,7 @@
 
             <!-- Sidebar -->
             <div class="sidebar">
-                <!-- Sidebar user (optional) -->
-                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div class="info">
-                        <a href="#" class="d-block"><?= $resultUser["username"] ?></a>
-                    </div>
-                </div>
-
-                <!-- SidebarSearch Form -->
-                <!-- Sidebar Menu -->
-                <nav class="mt-2">
-                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                         data-accordion="false">
                         <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
@@ -242,8 +232,6 @@
                             </a>
                         </li>
                     </ul>
-                </nav>
-                <!-- /.sidebar-menu -->
             </div>
             <!-- /.sidebar -->
         </aside>
